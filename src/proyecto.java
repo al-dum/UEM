28f8138b7b290c2dd1f8c4f772a86817edc6d9fe
Aldo <aldod.zamoral@gmail.com>
2024-12-09 17:21:40 +0100

    proyecto para fundmentos de programacion

--- src/proyecto.java
+++
@@ -1,7 +0,0 @@
-public class proyecto {
-
-
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/proyecto/proyecto.java
@@ -0,0 +1,17 @@
+package proyecto;
+
+public class proyecto {
+
+    String id = "";
+    int fecha;
+    int
+public static void app(){
+
+}
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
